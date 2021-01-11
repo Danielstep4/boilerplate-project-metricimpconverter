aeb3ef784446d9e6ad7e7c510b7e2dde41e228a3
Daniel Stepanishin <63352307+Danielstep4@users.noreply.github.com>
2021-01-11 15:29:04 +0200

    Completed API
    Completed API

--- routes/api.js
+++ routes/api.js
@@ -20,11 +20,23 @@ module.exports = function (app) {
       let input = req.query.input;
       let initNum = convertHandler.getNum(input);
       let initUnit = convertHandler.getUnit(input);
-      let returnNum = convertHandler.convert(initNum, initUnit);
-      let returnUnit = convertHandler.getReturnUnit(initUnit);
-      let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+      if(initNum === 'invalid number' && initUnit === 'invalid unit') {
+          res.send('invalid number and unit');
+      }else if(initNum === 'invalid number') {
+          res.send('invalid number')
+      }else if(initUnit === 'invalid unit') {
+          res.send('invalid unit')
+      }else {
+          let returnNum = convertHandler.convert(initNum, initUnit);
+          let returnUnit = convertHandler.getReturnUnit(initUnit);
+          let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+          res.json({
+            initNum,
+            initUnit,
+            returnNum,
+            returnUnit,
+            string: toString
+          })
+      }
     });
-    
 };
